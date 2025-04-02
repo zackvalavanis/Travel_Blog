60d84c9d998b7aa6404e48309f8b13e64c3f059f
Zack <168221270+Zval321@users.noreply.github.com>
2025-04-01 20:42:10 -0500

    created the CRUD for images controller

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,5 +1,53 @@
 class ImagesController < ApplicationController
   def index 
-    render json: { message: 'hello'}
+    @images = Image.all
+    render :index 
   end 
+
+  def show 
+    @image = Image.find_by(id: params[:id])
+    render :show 
+  end 
+
+  def create 
+    @image = Image.new(
+      image_url: params[:image_url], 
+      destination_id: params[:destination_id]
+    )
+    if @image.save 
+      render json: { message: 'The image has been saved'}, status: :created
+    else 
+      render json: { error: 'The image has not been created'}, status :bad_request
+    end 
+  end 
+
+  def update
+    @image = Image.find_by(id: params[:id])
+    if @image
+      if @image.update( 
+        image_url: params[:image_url] || @image.image_url, 
+        destination_id: params[:destination_id] || @image.destination_id
+      )
+      render json: { message: 'Image updated successfully'}, status: :ok
+      else 
+        render json: { error: @image.errors.full_messages}, status: :unprocessable_entity
+      end 
+    else 
+      render json: { error: 'Image not found'}, status: :not_found
+    end 
+  end 
+
+  def destroy 
+    @image = Image.find_by(id: params[:id])
+
+    if @image 
+      if @image.destroy 
+        render json: { message: 'The image has been deleted'}, status: :ok
+      else 
+        render json: { error: @image.errors.full_messages}, status: :unprocessable_entity
+      end 
+    else 
+      render json: { error: 'The image cannot be found'}, status: :not_found
+    end 
+  end
 end
