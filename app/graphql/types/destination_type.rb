fbe482c265fa1f86ec4a270dd45275040f803527
Zack <168221270+Zval321@users.noreply.github.com>
2025-06-10 18:45:13 -0500

    updated grapghQl of typing fr destinations to include more data

--- app/graphql/types/destination_type.rb
+++ app/graphql/types/destination_type.rb
@@ -3,5 +3,11 @@ module Types
     field :id, ID, null: false
     field :city, String, null:false
     field :country, String, null:false
+    field :description, String, null:false
+    field :image, [String], null:false
+
+    def image
+      object.images.map(&:image_url)
+    end
   end
 end
